4024d858243e7fbcf6bc4d3abe09ff23d9269288
boolyahoo <boolyahoo@gmail.com>
2016-06-04 09:30:29 +0800

    一个时间服务器的客户端

--- src/com/xcoder/Client.java
+++ src/com/xcoder/Client.java
@@ -1,19 +1,8 @@
 package com.xcoder;
 
 
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioSocketChannel;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
+import java.io.*;
+import java.net.Socket;
 
 /**
  * Netty异步通信框架客户端
@@ -21,35 +10,45 @@ import java.net.SocketAddress;
 public class Client {
     private static final String host = "localhost";
     private static final int port = 8080;
-    private PrintStream out = System.out;
-    private Channel channel;
 
 
     public void run() throws Exception {
-        EventLoopGroup group = new NioEventLoopGroup();
+        Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
         try {
-            Bootstrap bStrap = new Bootstrap()
-                    .group(group)
-                    .channel(NioSocketChannel.class)
-                    .handler(new ClientInitializer());
-            channel = bStrap.connect(host, port).sync().channel();
-            // 向master表明自己是client，并询问当前工作目录
-            byte type[] = {MSG.HEAD_CLIENT, MSG.CLIENT_QUERY_PWD};
-            channel.writeAndFlush(new String(type) + "\n");
-            BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
-            while (true) {
-                // 循环获取用户输入
-                String data = bReader.readLine();
-                if(data.contains("exit")){
-                    break;
-                }
-                channel.writeAndFlush(data + "\r\n");
-            }
+            socket = new Socket(host, port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            out.println("query time order");
+            System.out.println("sending order to server success");
+            String resp = in.readLine();
+            System.out.println("time is " + resp);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            group.shutdownGracefully();
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                in = null;
+            }
+            if (out != null) {
+                out.close();
+                out = null;
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                socket = null;
+            }
         }
+
     }
 
 
